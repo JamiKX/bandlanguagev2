84b687df35b6a52a07ef4c9858cc94f54c681fee
JamiKX <xjkycan@foxmail.com>
2020-10-26 19:33:00 +0800

    add

---
+++ src/com/common/antlrfile/commonParser.java
@@ -0,0 +1,420 @@
+package com.common.antlrfile;// Generated from common.g4 by ANTLR 4.8
+import org.antlr.v4.runtime.atn.*;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.*;
+import org.antlr.v4.runtime.tree.*;
+import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+@SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
+public class commonParser extends Parser {
+	static { RuntimeMetaData.checkVersion("4.8", RuntimeMetaData.VERSION); }
+
+	protected static final DFA[] _decisionToDFA;
+	protected static final PredictionContextCache _sharedContextCache =
+		new PredictionContextCache();
+	public static final int
+		Dot=1, Show=2, Execute=3, Delete=4, Search=5, Str=6, LeaveTool=7, PeopleName=8;
+	public static final int
+		RULE_script = 0, RULE_stmt = 1, RULE_verb = 2, RULE_obj = 3, RULE_simpleStmt = 4, 
+		RULE_actionStmt = 5, RULE_doSmtStmt = 6;
+	private static String[] makeRuleNames() {
+		return new String[] {
+			"script", "stmt", "verb", "obj", "simpleStmt", "actionStmt", "doSmtStmt"
+		};
+	}
+	public static final String[] ruleNames = makeRuleNames();
+
+	private static String[] makeLiteralNames() {
+		return new String[] {
+			null, null, "'\u93C4\u5267\u305A'", "'\u93B5\u0446\uE511'", "'\u9352\u72BB\u6ACE'", 
+			"'\u93CC\u30E6\u58D8'", "'helloworld'", "'\u7487\u5CF0\u4EA3\u5BB8\u30E5\u53FF'", 
+			"'\u5BEE\u72B1\u7B01'"
+		};
+	}
+	private static final String[] _LITERAL_NAMES = makeLiteralNames();
+	private static String[] makeSymbolicNames() {
+		return new String[] {
+			null, "Dot", "Show", "Execute", "Delete", "Search", "Str", "LeaveTool", 
+			"PeopleName"
+		};
+	}
+	private static final String[] _SYMBOLIC_NAMES = makeSymbolicNames();
+	public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
+
+	/**
+	 * @deprecated Use {@link #VOCABULARY} instead.
+	 */
+	@Deprecated
+	public static final String[] tokenNames;
+	static {
+		tokenNames = new String[_SYMBOLIC_NAMES.length];
+		for (int i = 0; i < tokenNames.length; i++) {
+			tokenNames[i] = VOCABULARY.getLiteralName(i);
+			if (tokenNames[i] == null) {
+				tokenNames[i] = VOCABULARY.getSymbolicName(i);
+			}
+
+			if (tokenNames[i] == null) {
+				tokenNames[i] = "<INVALID>";
+			}
+		}
+	}
+
+	@Override
+	@Deprecated
+	public String[] getTokenNames() {
+		return tokenNames;
+	}
+
+	@Override
+
+	public Vocabulary getVocabulary() {
+		return VOCABULARY;
+	}
+
+	@Override
+	public String getGrammarFileName() { return "common.g4"; }
+
+	@Override
+	public String[] getRuleNames() { return ruleNames; }
+
+	@Override
+	public String getSerializedATN() { return _serializedATN; }
+
+	@Override
+	public ATN getATN() { return _ATN; }
+
+	public commonParser(TokenStream input) {
+		super(input);
+		_interp = new ParserATNSimulator(this,_ATN,_decisionToDFA,_sharedContextCache);
+	}
+
+	public static class ScriptContext extends ParserRuleContext {
+		public List<StmtContext> stmt() {
+			return getRuleContexts(StmtContext.class);
+		}
+		public StmtContext stmt(int i) {
+			return getRuleContext(StmtContext.class,i);
+		}
+		public List<TerminalNode> Dot() { return getTokens(commonParser.Dot); }
+		public TerminalNode Dot(int i) {
+			return getToken(commonParser.Dot, i);
+		}
+		public ScriptContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_script; }
+		@Override
+		public <T> T accept(ParseTreeVisitor<? extends T> visitor) {
+			if ( visitor instanceof commonVisitor ) return ((commonVisitor<? extends T>)visitor).visitScript(this);
+			else return visitor.visitChildren(this);
+		}
+	}
+
+	public final ScriptContext script() throws RecognitionException {
+		ScriptContext _localctx = new ScriptContext(_ctx, getState());
+		enterRule(_localctx, 0, RULE_script);
+		int _la;
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(18); 
+			_errHandler.sync(this);
+			_la = _input.LA(1);
+			do {
+				{
+				{
+				setState(14);
+				stmt();
+				setState(16);
+				_errHandler.sync(this);
+				_la = _input.LA(1);
+				if (_la==Dot) {
+					{
+					setState(15);
+					match(Dot);
+					}
+				}
+
+				}
+				}
+				setState(20); 
+				_errHandler.sync(this);
+				_la = _input.LA(1);
+			} while ( (((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << Show) | (1L << Execute) | (1L << Delete) | (1L << Search))) != 0) );
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class StmtContext extends ParserRuleContext {
+		public SimpleStmtContext simpleStmt() {
+			return getRuleContext(SimpleStmtContext.class,0);
+		}
+		public StmtContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_stmt; }
+		@Override
+		public <T> T accept(ParseTreeVisitor<? extends T> visitor) {
+			if ( visitor instanceof commonVisitor ) return ((commonVisitor<? extends T>)visitor).visitStmt(this);
+			else return visitor.visitChildren(this);
+		}
+	}
+
+	public final StmtContext stmt() throws RecognitionException {
+		StmtContext _localctx = new StmtContext(_ctx, getState());
+		enterRule(_localctx, 2, RULE_stmt);
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(22);
+			simpleStmt();
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class VerbContext extends ParserRuleContext {
+		public TerminalNode Show() { return getToken(commonParser.Show, 0); }
+		public TerminalNode Execute() { return getToken(commonParser.Execute, 0); }
+		public TerminalNode Delete() { return getToken(commonParser.Delete, 0); }
+		public TerminalNode Search() { return getToken(commonParser.Search, 0); }
+		public VerbContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_verb; }
+		@Override
+		public <T> T accept(ParseTreeVisitor<? extends T> visitor) {
+			if ( visitor instanceof commonVisitor ) return ((commonVisitor<? extends T>)visitor).visitVerb(this);
+			else return visitor.visitChildren(this);
+		}
+	}
+
+	public final VerbContext verb() throws RecognitionException {
+		VerbContext _localctx = new VerbContext(_ctx, getState());
+		enterRule(_localctx, 4, RULE_verb);
+		int _la;
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(24);
+			_la = _input.LA(1);
+			if ( !((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << Show) | (1L << Execute) | (1L << Delete) | (1L << Search))) != 0)) ) {
+			_errHandler.recoverInline(this);
+			}
+			else {
+				if ( _input.LA(1)==Token.EOF ) matchedEOF = true;
+				_errHandler.reportMatch(this);
+				consume();
+			}
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class ObjContext extends ParserRuleContext {
+		public TerminalNode Str() { return getToken(commonParser.Str, 0); }
+		public TerminalNode LeaveTool() { return getToken(commonParser.LeaveTool, 0); }
+		public TerminalNode PeopleName() { return getToken(commonParser.PeopleName, 0); }
+		public ObjContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_obj; }
+		@Override
+		public <T> T accept(ParseTreeVisitor<? extends T> visitor) {
+			if ( visitor instanceof commonVisitor ) return ((commonVisitor<? extends T>)visitor).visitObj(this);
+			else return visitor.visitChildren(this);
+		}
+	}
+
+	public final ObjContext obj() throws RecognitionException {
+		ObjContext _localctx = new ObjContext(_ctx, getState());
+		enterRule(_localctx, 6, RULE_obj);
+		int _la;
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(26);
+			_la = _input.LA(1);
+			if ( !((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << Str) | (1L << LeaveTool) | (1L << PeopleName))) != 0)) ) {
+			_errHandler.recoverInline(this);
+			}
+			else {
+				if ( _input.LA(1)==Token.EOF ) matchedEOF = true;
+				_errHandler.reportMatch(this);
+				consume();
+			}
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class SimpleStmtContext extends ParserRuleContext {
+		public ActionStmtContext actionStmt() {
+			return getRuleContext(ActionStmtContext.class,0);
+		}
+		public SimpleStmtContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_simpleStmt; }
+		@Override
+		public <T> T accept(ParseTreeVisitor<? extends T> visitor) {
+			if ( visitor instanceof commonVisitor ) return ((commonVisitor<? extends T>)visitor).visitSimpleStmt(this);
+			else return visitor.visitChildren(this);
+		}
+	}
+
+	public final SimpleStmtContext simpleStmt() throws RecognitionException {
+		SimpleStmtContext _localctx = new SimpleStmtContext(_ctx, getState());
+		enterRule(_localctx, 8, RULE_simpleStmt);
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(28);
+			actionStmt();
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class ActionStmtContext extends ParserRuleContext {
+		public DoSmtStmtContext doSmtStmt() {
+			return getRuleContext(DoSmtStmtContext.class,0);
+		}
+		public ActionStmtContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_actionStmt; }
+		@Override
+		public <T> T accept(ParseTreeVisitor<? extends T> visitor) {
+			if ( visitor instanceof commonVisitor ) return ((commonVisitor<? extends T>)visitor).visitActionStmt(this);
+			else return visitor.visitChildren(this);
+		}
+	}
+
+	public final ActionStmtContext actionStmt() throws RecognitionException {
+		ActionStmtContext _localctx = new ActionStmtContext(_ctx, getState());
+		enterRule(_localctx, 10, RULE_actionStmt);
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(30);
+			doSmtStmt();
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class DoSmtStmtContext extends ParserRuleContext {
+		public VerbContext verb() {
+			return getRuleContext(VerbContext.class,0);
+		}
+		public ObjContext obj() {
+			return getRuleContext(ObjContext.class,0);
+		}
+		public DoSmtStmtContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_doSmtStmt; }
+		@Override
+		public <T> T accept(ParseTreeVisitor<? extends T> visitor) {
+			if ( visitor instanceof commonVisitor ) return ((commonVisitor<? extends T>)visitor).visitDoSmtStmt(this);
+			else return visitor.visitChildren(this);
+		}
+	}
+
+	public final DoSmtStmtContext doSmtStmt() throws RecognitionException {
+		DoSmtStmtContext _localctx = new DoSmtStmtContext(_ctx, getState());
+		enterRule(_localctx, 12, RULE_doSmtStmt);
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(32);
+			verb();
+			setState(33);
+			obj();
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static final String _serializedATN =
+		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\n&\4\2\t\2\4\3\t"+
+		"\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7\4\b\t\b\3\2\3\2\5\2\23\n\2\6\2\25\n"+
+		"\2\r\2\16\2\26\3\3\3\3\3\4\3\4\3\5\3\5\3\6\3\6\3\7\3\7\3\b\3\b\3\b\3\b"+
+		"\2\2\t\2\4\6\b\n\f\16\2\4\3\2\4\7\3\2\b\n\2 \2\24\3\2\2\2\4\30\3\2\2\2"+
+		"\6\32\3\2\2\2\b\34\3\2\2\2\n\36\3\2\2\2\f \3\2\2\2\16\"\3\2\2\2\20\22"+
+		"\5\4\3\2\21\23\7\3\2\2\22\21\3\2\2\2\22\23\3\2\2\2\23\25\3\2\2\2\24\20"+
+		"\3\2\2\2\25\26\3\2\2\2\26\24\3\2\2\2\26\27\3\2\2\2\27\3\3\2\2\2\30\31"+
+		"\5\n\6\2\31\5\3\2\2\2\32\33\t\2\2\2\33\7\3\2\2\2\34\35\t\3\2\2\35\t\3"+
+		"\2\2\2\36\37\5\f\7\2\37\13\3\2\2\2 !\5\16\b\2!\r\3\2\2\2\"#\5\6\4\2#$"+
+		"\5\b\5\2$\17\3\2\2\2\4\22\26";
+	public static final ATN _ATN =
+		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
+	static {
+		_decisionToDFA = new DFA[_ATN.getNumberOfDecisions()];
+		for (int i = 0; i < _ATN.getNumberOfDecisions(); i++) {
+			_decisionToDFA[i] = new DFA(_ATN.getDecisionState(i), i);
+		}
+	}
+}
---
+++ src/com/common/antlrfile/commonVisitor.java
@@ -0,0 +1,54 @@
+package com.common.antlrfile;// Generated from common.g4 by ANTLR 4.8
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link commonParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface commonVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link commonParser#script}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitScript(commonParser.ScriptContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link commonParser#stmt}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitStmt(commonParser.StmtContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link commonParser#verb}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitVerb(commonParser.VerbContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link commonParser#obj}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitObj(commonParser.ObjContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link commonParser#simpleStmt}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitSimpleStmt(commonParser.SimpleStmtContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link commonParser#actionStmt}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitActionStmt(commonParser.ActionStmtContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link commonParser#doSmtStmt}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDoSmtStmt(commonParser.DoSmtStmtContext ctx);
+}
